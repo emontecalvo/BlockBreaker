96f0a3a349470b324b11eb67ebf606250c6ec4e4
emontecalvo <emilymontecalvo@gmail.com>
2018-01-09 15:31:02 -0800

    inital commit

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+	static MusicPlayer instance = null;
+
+	void Awake() {
+//		Debug.Log ("Music player Awake " + GetInstanceID ());
+		if (instance != null) {
+			Destroy (gameObject);
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad (gameObject);		
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
