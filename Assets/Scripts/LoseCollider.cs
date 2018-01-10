d20c9022986f76ca4cf1bbc108c5daeee9f3f496
emontecalvo <emilymontecalvo@gmail.com>
2018-01-10 12:41:29 -0800

    setting up loading level

--- Assets/Scripts/LoseCollider.cs
+++ Assets/Scripts/LoseCollider.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class LoseCollider : MonoBehaviour {
 
+	public LevelManager levelManager;
+
 	void OnTriggerEnter2D (Collider2D trigger) {
 		print ("trigger");
+		levelManager.LoadLevel ("Win Screen");
+
 	}
 
 	void OnCollisionEnter2D (Collision2D collision) {
